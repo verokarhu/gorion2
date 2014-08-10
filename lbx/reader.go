11daa5d15553470d4fbb8cf40e08dc4866288f43
verokarhu <andreas.metsala@gmail.com>
2014-08-10 13:19:18 +0300

    started work on lbx reader, supports smacker files for now

---
+++ lbx/reader.go
@@ -0,0 +1,52 @@
+package lbx
+
+import (
+	"io"
+	"io/ioutil"
+)
+
+type decoder struct {
+	r   io.Reader
+	tmp [4096]byte
+}
+
+const (
+	filetypeNone = iota
+	filetypeSmacker
+	filetypeImage
+	filetypeSound
+)
+
+func (d *decoder) processHeader() (int, error) {
+	if _, err := io.ReadFull(d.r, d.tmp[:12]); err != nil {
+		return filetypeNone, err
+	}
+
+	if string(d.tmp[0:3]) == "SMK" {
+		return filetypeSmacker, nil
+	}
+
+	return filetypeNone, nil
+}
+
+// Decode reads the contents of an lbx file and returns the files
+func Decode(r io.Reader, name string) (map[string][]byte, error) {
+	d := &decoder{r: r}
+	t, err := d.processHeader()
+
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string][]byte)
+	switch t {
+	case filetypeSmacker:
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			return nil, err
+		}
+		m[name+".1.smk"] = append(d.tmp[:12], b[:]...)
+	}
+
+	return m, nil
+}
---
+++ lbx/reader_test.go
@@ -0,0 +1,52 @@
+package lbx
+
+import (
+	"bytes"
+	"testing"
+)
+
+var (
+	smacker  = []byte("SMK4datadatadata")
+	garbage  = []byte("garbage")
+	filename = "filename"
+)
+
+func TestProcessHeader_Smacker(t *testing.T) {
+	r := bytes.NewReader(smacker)
+	d := decoder{r: r}
+
+	filetype, err := d.processHeader()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if filetype != filetypeSmacker {
+		t.Error("wrong type")
+	}
+}
+
+func TestProcessHeader_Garbage(t *testing.T) {
+	r := bytes.NewReader(garbage)
+	d := decoder{r: r}
+
+	if filetype, _ := d.processHeader(); filetype != filetypeNone {
+		t.Error("wrong type")
+	}
+}
+
+func TestDecode_Smacker(t *testing.T) {
+	r := bytes.NewReader(smacker)
+	m, err := Decode(r, filename)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if l := len(m); l != 1 {
+		t.Error("expected 1 result, returned", l)
+	}
+
+	if bytes.Compare(m[filename+".1.smk"], smacker) != 0 {
+		t.Error("excepted ", smacker, ", returned ", m[filename+".1.smk"])
+	}
+}
