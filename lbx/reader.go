c8577d34cb2ec9a08b3206c790b962ec70d5e21d
verokarhu <andreas.metsala@gmail.com>
2014-08-10 20:37:52 +0300

    support for wav files in lbx

--- lbx/reader.go
+++ lbx/reader.go
@@ -1,51 +1,83 @@
 package lbx
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"io"
 	"io/ioutil"
 )
 
-type decoder struct {
-	r   io.Reader
-	tmp [4096]byte
+type lbxHeader struct {
+	NumEntries uint16
+	Magic      uint16
+	Reserved   uint16
+	FileType   uint16
+	Offsets    []uint32
 }
 
-const (
-	filetypeNone = iota
-	filetypeSmacker
-	filetypeImage
-	filetypeSound
-)
-
-func (d *decoder) processHeader() (int, error) {
-	if _, err := io.ReadFull(d.r, d.tmp[:12]); err != nil {
-		return filetypeNone, err
-	}
-
-	if string(d.tmp[0:3]) == "SMK" {
-		return filetypeSmacker, nil
-	}
-
-	return filetypeNone, nil
-}
-
-// Decode reads the contents of an lbx file, decodes the contained files and returns readers that contain the converted files
-func Decode(r io.Reader, name string) (map[string][]byte, error) {
-	d := &decoder{r: r}
-	t, err := d.processHeader()
-
+func processFile(r io.Reader) (map[string][]byte, error) {
+	b, err := ioutil.ReadAll(r)
 	if err != nil {
 		return nil, err
 	}
 
 	m := make(map[string][]byte)
-	switch t {
-	case filetypeSmacker:
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			return nil, err
+	if string(b[0:3]) == "SMK" {
+		m["1.smk"] = b
+		return m, nil
+	}
+
+	lbxHeader := processLbxHeader(b)
+
+	if lbxHeader.Magic == 65197 {
+		for i := 0; i < int(lbxHeader.NumEntries); i++ {
+			offset := lbxHeader.Offsets[i]
+			if string(b[offset:offset+4]) == "RIFF" {
+				k := fmt.Sprintf("%d.wav", i+1)
+
+				if int(lbxHeader.NumEntries) == i+1 {
+					m[k] = b[offset:]
+				} else {
+					endoffset := lbxHeader.Offsets[i+1]
+					m[k] = b[offset:endoffset]
+				}
+			}
 		}
-		m[name+".1.smk"] = append(d.tmp[:12], b[:]...)
+
+		return m, nil
+	}
+
+	return nil, nil
+}
+
+func processLbxHeader(b []byte) lbxHeader {
+	buf := bytes.NewReader(b)
+	r := lbxHeader{}
+
+	binary.Read(buf, binary.LittleEndian, &r.NumEntries)
+	binary.Read(buf, binary.LittleEndian, &r.Magic)
+	binary.Read(buf, binary.LittleEndian, &r.Reserved)
+	binary.Read(buf, binary.LittleEndian, &r.FileType)
+
+	r.Offsets = make([]uint32, r.NumEntries, r.NumEntries)
+	for i := 0; i < int(r.NumEntries); i++ {
+		binary.Read(buf, binary.LittleEndian, &r.Offsets[i])
+	}
+
+	return r
+}
+
+// Decode reads the contents of an lbx file, decodes the contained files and returns readers that contain the converted files
+func Decode(r io.Reader, name string) (map[string][]byte, error) {
+	files, err := processFile(r)
+	if err != nil {
+		return nil, err
+	}
+
+	m := make(map[string][]byte)
+	for k, v := range files {
+		m[name+"."+k] = v
 	}
 
 	return m, nil
--- lbx/reader_test.go
+++ lbx/reader_test.go
@@ -8,33 +8,102 @@ import (
 var (
 	smacker  = []byte("SMK4datadatadata")
 	garbage  = []byte("garbage")
+	audio    = []byte{2, 0, 173, 254, 0, 0, 0, 0, 16, 0, 0, 0, 20, 0, 0, 0, 0x52, 0x49, 0x46, 0x46, 0x52, 0x49, 0x46, 0x46}
 	filename = "filename"
 )
 
-func TestProcessHeader_Smacker(t *testing.T) {
+func TestProcessFile_Smacker(t *testing.T) {
 	r := bytes.NewReader(smacker)
-	d := decoder{r: r}
 
-	filetype, err := d.processHeader()
+	m, err := processFile(r)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if filetype != filetypeSmacker {
-		t.Error("wrong type")
+	if l := len(m); l != 1 {
+		t.Error("excepted 1, returned ", l)
+	}
+
+	if bytes.Compare(m["1.smk"], smacker) != 0 {
+		t.Error("excepted ", smacker, ", returned ", m["1.smk"])
 	}
 }
 
-func TestProcessHeader_Garbage(t *testing.T) {
+func TestProcessFile_Garbage(t *testing.T) {
 	r := bytes.NewReader(garbage)
-	d := decoder{r: r}
 
-	if filetype, _ := d.processHeader(); filetype != filetypeNone {
-		t.Error("wrong type")
+	m, err := processFile(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if l := len(m); l != 0 {
+		t.Error("excepted 0, returned ", l)
 	}
 }
 
-func TestDecode_Smacker(t *testing.T) {
+func TestProcessFile_Audio(t *testing.T) {
+	r := bytes.NewReader(audio)
+
+	m, err := processFile(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if l := len(m); l != 2 {
+		t.Error("excepted 2, returned ", l)
+	}
+
+	if r := "1.wav"; m[r] == nil {
+		t.Error("excepted ", r, " to exist")
+	}
+
+	if r := "2.wav"; m[r] == nil {
+		t.Error("excepted ", r, " to exist")
+	}
+
+	if r := m["1.wav"]; bytes.Compare(r, audio[16:20]) != 0 {
+		t.Error("excepted ", audio[16:20], ", returned ", r)
+	}
+
+	if r := m["2.wav"]; bytes.Compare(r, audio[20:]) != 0 {
+		t.Error("excepted ", audio[20:], ", returned ", r)
+	}
+}
+
+func TestProcessLbxHeader(t *testing.T) {
+	lbx := processLbxHeader(audio)
+
+	if r := lbx.NumEntries; r != 2 {
+		t.Error("expected 2, returned", r)
+	}
+
+	if r := lbx.Magic; r != 65197 {
+		t.Error("expected 65197, returned", r)
+	}
+
+	if r := lbx.Reserved; r != 0 {
+		t.Error("expected 0, returned", r)
+	}
+
+	if r := lbx.FileType; r != 0 {
+		t.Error("expected 0, returned", r)
+	}
+
+	if l := len(lbx.Offsets); l != 2 {
+		t.Error("expected 2 results, returned", l)
+	}
+
+	if r := lbx.Offsets[0]; r != 16 {
+		t.Error("expected 15, returned", r)
+	}
+
+	if r := lbx.Offsets[1]; r != 20 {
+		t.Error("expected 19, returned", r)
+	}
+}
+
+func TestDecode(t *testing.T) {
 	r := bytes.NewReader(smacker)
 	m, err := Decode(r, filename)
 
@@ -42,11 +111,13 @@ func TestDecode_Smacker(t *testing.T) {
 		t.Error(err)
 	}
 
-	if l := len(m); l != 1 {
-		t.Error("expected 1 result, returned", l)
-	}
+	for k, v := range m {
+		if k != filename+".1.smk" {
+			t.Error("excepted ", filename, ".1.smk, returned ", k)
+		}
 
-	if bytes.Compare(m[filename+".1.smk"], smacker) != 0 {
-		t.Error("excepted ", smacker, ", returned ", m[filename+".1.smk"])
+		if bytes.Compare(v, smacker) != 0 {
+			t.Error("excepted ", smacker, ", returned ", v)
+		}
 	}
 }
