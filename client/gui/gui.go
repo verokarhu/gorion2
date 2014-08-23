9c5d6247977f7ab9103bbdb68d0c585c785f7ee3
verokarhu <andreas.metsala@gmail.com>
2014-08-24 01:52:55 +0300

    buttonmap for handling clickable areas

--- client/gui/gui.go
+++ client/gui/gui.go
@@ -4,10 +4,23 @@ import (
 	sf "github.com/verokarhu/gorion2/third_party/bitbucket.org/krepa098/gosfml2"
 )
 
+type ButtonMap struct {
+	UpperLeft  sf.Vector2f
+	LowerRight sf.Vector2f
+}
+
 // CalcPosVec calculates a resolution independent vector of the x,y position
-func CalcPosVec(pos sf.Vector2u, res sf.Vector2u) (v sf.Vector2f) {
+func CalcPosVec(pos sf.Vector2i, res sf.Vector2u) (v sf.Vector2f) {
 	v.X = float32(pos.X) / float32(res.X)
 	v.Y = float32(pos.Y) / float32(res.Y)
 
 	return
+}
+
+func (b *ButtonMap) Contains(pos sf.Vector2f) bool {
+	if pos.X >= b.UpperLeft.X && pos.X <= b.LowerRight.X && pos.Y >= b.UpperLeft.Y && pos.Y <= b.LowerRight.Y {
+		return true
+	}
+
+	return false
 }
--- client/gui/gui_test.go
+++ client/gui/gui_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test_CalcPosVec(t *testing.T) {
-	p := CalcPosVec(sf.Vector2u{1, 1}, sf.Vector2u{4, 2})
+	p := CalcPosVec(sf.Vector2i{1, 1}, sf.Vector2u{4, 2})
 
 	if p.X != 0.25 {
 		t.Error("X was ", p.X)
@@ -15,5 +15,37 @@ func Test_CalcPosVec(t *testing.T) {
 
 	if p.Y != 0.5 {
 		t.Error("Y was ", p.Y)
+	}
+}
+
+func Test_ButtonMap_Contains(t *testing.T) {
+	bm := ButtonMap{sf.Vector2f{0.2, 0.2}, sf.Vector2f{0.3, 0.3}}
+
+	if bm.Contains(sf.Vector2f{0.1, 0.25}) {
+		t.Error("returned true")
+	}
+
+	if bm.Contains(sf.Vector2f{0.4, 0.25}) {
+		t.Error("returned true")
+	}
+
+	if bm.Contains(sf.Vector2f{0.25, 0.1}) {
+		t.Error("returned true")
+	}
+
+	if bm.Contains(sf.Vector2f{0.1, 0.4}) {
+		t.Error("returned true")
+	}
+
+	if !bm.Contains(sf.Vector2f{0.2, 0.2}) {
+		t.Error("returned false")
+	}
+
+	if !bm.Contains(sf.Vector2f{0.25, 0.25}) {
+		t.Error("returned false")
+	}
+
+	if !bm.Contains(sf.Vector2f{0.3, 0.3}) {
+		t.Error("returned false")
 	}
 }
