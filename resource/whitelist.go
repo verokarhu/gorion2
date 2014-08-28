5a401597829fca9fedb2fed92df38d006fcf13a4
verokarhu <andreas.metsala@gmail.com>
2014-08-28 23:51:18 +0300

    update whitelist

--- resource/whitelist.go
+++ resource/whitelist.go
@@ -2,8 +2,8 @@ package resource
 
 // list of files that get used
 var whitelist = []string{
-	"mainmenu_21_f1.png",
-	"tanm_001_0_f30.png",
+	"mainmenu_21_f1_d0.png",
+	"tanm_001_0_f30_d0.png",
 	"stream3.wav",
 	"streamhd17.wav",
 }
